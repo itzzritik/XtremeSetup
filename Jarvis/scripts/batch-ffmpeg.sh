d31bce3344ab7e8a643d7e2ab4c9635e8150e036
Ritik Srivastava <ritik.space@gmail.com>
2023-05-10 17:51:38 +0530

    Nice progress bar for ffmpeg

--- Jarvis/scripts/batch-ffmpeg.sh
+++ Jarvis/scripts/batch-ffmpeg.sh
@@ -7,10 +7,19 @@ then
     exit 1
 fi
 
-DIRECTORY="/media/drive1/Public/Movies/Marvel Cinematic Universe"
+DIRECTORY="/media/drive1/Downloads/Backup"
 ALREADY_CONVERTED=()
 
 echo "⚪ Coverting MKV Movies to MP4"
+echo
+# Install pv if not installed already
+if ! [[ $(which pv) && $(pv --version) ]];
+then
+    echo "⛔ Package \"pv\" not found, Installing..."
+    echo
+    sudo apt install pv -y
+    echo
+fi
 echo
 echo "→ Scanning for Movies in directory ($DIRECTORY)"
 
@@ -29,40 +38,47 @@ done
 echo
 echo "→ Converting the discovered movies"
 echo
+
+progressbar() {
+  local width=50
+  local percent=$1
+  local progress=$(( width * percent / 100 ))
+
+  # Set color codes
+  local color_complete="\e[92m"
+  local color_incomplete="\e[37m"
+  local color_reset="\e[0m"
+
+  # Print progress bar
+  printf "\r"
+  printf "${color_complete}$(printf '█%.0s' $(seq 1 "$progress"))${color_incomplete}$(printf ' %.0s' $(seq 1 "$((width-progress))"))${color_reset}"
+  printf " %3d%%" "$percent"
+}
+
 for FILE in "$DIRECTORY"/**/*.mkv;
 do
     if ! [ -f "${FILE%.*}.mp4" ]
     then
         echo "- Converting $FILE"
         echo
-        
-        # ffmpeg -v quiet -stats -i "$FILE" -c copy "${FILE%.*}.mp4"
-        # Get video duration in frames
-        duration=$(ffmpeg -i "$FILE" 2>&1 | sed -n "s/.* Duration: \([^,]*\), start: .*/\1/p")
-        fps=$(ffmpeg -i "$FILE" 2>&1 | sed -n "s/.*, \(.*\) tbr.*/\1/p")
-        hours=$(echo $duration | cut -d":" -f1)
-        minutes=$(echo $duration | cut -d":" -f2)
-        seconds=$(echo $duration | cut -d":" -f3)
-        FRAMES=$(echo "($hours*3600+$minutes*60+$seconds)*$fps" | bc | cut -d"." -f1)
-        
-        # Start ffmpeg, use awk to flush the buffer and remove carriage returns
-	    ffmpeg -v quiet -i "$FILE" -c copy "${FILE%.*}.mp4" | awk '1;{fflush()}' RS='\r\n'>/logs/ffmpeg &
 
-        # Get ffmpeg Process ID
-	    PID=$( ps -ef | grep "ffmpeg" | grep -v "grep" | awk '{print $2}' )
-        echo $PID
-        # While ffmpeg runs, process the log file for the current frame, display percentage progress
-        while ps -p $PID>/dev/null  ; do
-            currentframe=$(tail -n 1 /logs/ffmpeg | awk '/frame=/ { print $2 }')
-            echo $currentframe
-            if [[ -n "$currentframe" ]];
-            then
-                PROG=$(echo "scale=3; ($currentframe/$FRAMES)*100.0" | bc)
-                echo PROGRESS: $PROG
-                sleep 1
-            fi
+        INPUT="$FILE"
+        OUTPUT="${FILE%.*}.mp4"
+        INPUT_FILE_SIZE=$(du -b "$INPUT" | awk '{print $1}')
+
+        ffmpeg -i "$INPUT" -c copy "$OUTPUT" >/dev/null 2>&1 &
+        pid=$!  # Get the PID of the ffmpeg process
+        progress=0
+        while kill -0 $pid >/dev/null 2>&1;
+        do
+            PROGRESS_FILE_SIZE=$(stat -c "%s" "$OUTPUT" 2>/dev/null || echo 0)
+            PROGRESS=$(echo "($PROGRESS_FILE_SIZE * 100) / $INPUT_FILE_SIZE" | bc)
+            progressbar "$PROGRESS"
+            sleep 0.5
         done
-        mv "$FILE" "$FILE.bak"
+        
+        # Move original file to backup
+        # mv "$FILE" "$FILE.bak"
         echo
         echo "✔ Movie converted successfully"
         echo
