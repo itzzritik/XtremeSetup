3f6be5430301ee9a7dd3d3e4574a1b47a2769aba
Ritik Srivastava <ritik.space@gmail.com>
2023-05-18 12:38:46 +0530

    Add new progress bar functions

--- Jarvis/scripts/batch-ffmpeg.sh
+++ Jarvis/scripts/batch-ffmpeg.sh
@@ -7,7 +7,8 @@ then
     exit 1
 fi
 
-DIRECTORY="/media/drive1/Downloads/Backup"
+ROOT="/media/drive1"
+DIRECTORY="$ROOT/Public/Movies/Bollywood"
 ALREADY_CONVERTED=()
 
 echo "⚪ Coverting MKV Movies to MP4"
@@ -25,7 +26,6 @@ echo "→ Scanning for Movies in directory ($DIRECTORY)"
 
 shopt -s globstar nocaseglob nocasematch  # enable ** and globs case-insensitivity
 
-echo
 echo "→ Discovered the following movies in MKV Format:"
 echo
 for FILE in "$DIRECTORY"/**/*.mkv;
@@ -57,7 +57,28 @@ function SHOW_PROGRESS_BAR {
     todo_sub_bar=$(printf "%-${todo}s" | sed "s/ /$bar_char_todo/g")
 
     # output the bar
-    printf "\r${done_sub_bar}${todo_sub_bar} ${percent}%%"
+    printf "${done_sub_bar}${todo_sub_bar} ${percent}%%"
+}
+// ◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡◠◡●●⦿➒❥▻
+function SHOW_PROGRESS_BAR {
+    local current="$1"
+    local total="$2"
+    local bar_size=100
+    local bar_char_done="█"
+    local bar_char_todo="░"
+    local bar_percentage_scale=2
+    local percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+    
+    # The number of done and todo characters
+    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
+    todo=$(bc <<< "scale=0; $bar_size - $done" )
+
+    # build the done and todo sub-bars
+    done_sub_bar=$(printf "%-${done}s" | sed "s/ /$bar_char_done/g")
+    todo_sub_bar=$(printf "%-${todo}s" | sed "s/ /$bar_char_todo/g")
+
+    # output the bar
+    printf "${done_sub_bar}${todo_sub_bar} ${percent}%%"
 }
 
 for FILE in "$DIRECTORY"/**/*.mkv;
@@ -71,17 +92,20 @@ do
         OUTPUT="${FILE%.*}.mp4"
         INPUT_FILE_SIZE=$(du -b "$INPUT" | awk '{print $1}')
 
+        # Convert movie using ffmpeg
         ffmpeg -i "$INPUT" -c copy "$OUTPUT" >/dev/null 2>&1 &
-        pid=$!  # Get the PID of the ffmpeg process
+        pid=$!
         while kill -0 $pid >/dev/null 2>&1;
         do
             PROGRESS_FILE_SIZE=$(stat -c "%s" "$OUTPUT" 2>/dev/null || echo 0)
             SHOW_PROGRESS_BAR $PROGRESS_FILE_SIZE $INPUT_FILE_SIZE
             sleep 0.5
+            printf "\r"
         done
         
         #Original file to backup file
-        mv "$FILE" "$FILE.bak"
+        FILENAME=$(basename -- "$FILE")
+        mv "$FILE" "$ROOT/Downloads/Backup/$FILENAME.bak"
 
         echo
         echo "✔ Movie converted successfully"
