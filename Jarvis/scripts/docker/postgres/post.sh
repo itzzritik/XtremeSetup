c1d1f28393167decd7427477449917518d16d61b
Jarvis <ritik.jarvis@gmail.com>
2024-09-17 19:02:47 +0530

    ✨ Update postgres post scripts

--- Jarvis/scripts/docker/postgres/post.sh
+++ Jarvis/scripts/docker/postgres/post.sh
@@ -3,16 +3,19 @@ echo -e "\n\n● Post script for ${JARVIS_CONTAINER_NAME}\n"
 export PGPASSWORD=${JARVIS_POSTGRES_PASSWORD}
 
 echo "→ Waiting for ${JARVIS_CONTAINER_NAME}"
-until docker exec "${JARVIS_CONTAINER_NAME}" pg_isready -U "${JARVIS_ADMIN_USERNAME}" > /dev/null 2>&1; do
-  sleep 1
+until docker exec "${JARVIS_CONTAINER_NAME}" pg_isready -U "${JARVIS_ADMIN_USERNAME}" >/dev/null 2>&1; do
+	sleep 1
 done
 
-echo "✔ Create database → auth"
-DB_NAME="auth"
+DB_LIST=(
+	auth
+)
 
-if ! docker exec ${JARVIS_CONTAINER_NAME} psql -U ${JARVIS_ADMIN_USERNAME} -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" | grep -q 1; then
-  docker exec ${JARVIS_CONTAINER_NAME} psql -U ${JARVIS_ADMIN_USERNAME} -d postgres -c "CREATE DATABASE $DB_NAME;"
-  echo "✔ Database $DB_NAME created"
-else
-  echo "✔ Database $DB_NAME already exists"
-fi
+echo "→ Creating databases"
+for DB_NAME in "${DB_LIST[@]}"; do
+	if docker exec ${JARVIS_CONTAINER_NAME} psql -U ${JARVIS_ADMIN_USERNAME} -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" | grep -q 1; then
+		echo "✔ Database $DB_NAME already exists" && continue
+	fi
+	docker exec ${JARVIS_CONTAINER_NAME} psql -U ${JARVIS_ADMIN_USERNAME} -d postgres -c "CREATE DATABASE $DB_NAME;"
+	echo "✔ Database $DB_NAME created"
+done
